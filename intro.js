1200f82f9b7b794e73464cc45fccbbf1ff8f2f0d
Windaniel <godwinkondo@gmail.com>
2026-06-13 15:54:25 +0000

    Initial commit: Introduction to JS

---
+++ intro.js
@@ -0,0 +1,5 @@
+function intro (name, age, favProg) {
+    return 'my name is :'+' '+ name +' '+ 'I am '+' '+ age  +' '+'years old, my favorite programing language is:'+' '+ favProg
+}
+
+console.log (intro("Godwin", 20, 'Python'))
